319e190b0a4f8364a620e5425ae78b46016eeb1d
Arnob Das <adas46@uwo.ca>
2020-11-02 22:28:49 -0500

    Created second route for GET request to get all course codes given a subject

--- index.js
+++ index.js
@@ -38,6 +38,29 @@ app.get('/api/courses', (req, res) => {
     res.send(subjects);
 });
 
+// Route to GET all course codes for a given subject code -- #2
+app.get('/api/courses/subjects/:subject', (req, res) => {
+    const courseCode = [];
+    for(i = 0; i < courseData.length; i++) {
+        if (String(courseData[i].subject).includes(`${req.params.subject}`) && String(req.params.subject).length <= 15) {
+            courseCode.push({
+                "subject": courseData[i].subject,
+                "className": courseData[i].className,
+                "catalog_nbr": courseData[i].catalog_nbr,
+                "course_info": courseData[i].course_info,
+                "description": courseData[i].catalog_description
+            });
+        }
+    }
+    if (courseCode.length > 0) {
+        res.send(courseCode);
+    }
+    // 404 subject code doesn't exist
+    else {
+        res.status(404).send('The courses with the given subject code were not found.');
+    }
+});
+
 // PORT
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
