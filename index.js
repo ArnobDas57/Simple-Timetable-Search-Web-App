5f3f193ab5c685fe3edcec79535db814f895b7b8
Arnob Das <adas46@uwo.ca>
2020-11-02 22:21:48 -0500

    Created function to retrieve Lab3-timetable-data.json

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const port = 3000;
 
 app.use(express.json());
 
+//Function to parse JSON file
+function parsingFunction (datafile) {
+    const file = require('./' + datafile);
+    return file;
+}
+
+const courseData = parsingFunction('Lab3-timetable-data.json'); 
+
+// Setup serving front-end code
+app.use('/', express.static('static'));
+
+
+// Route for root 
+app.get('/', (req, res) => {
+    res.send('Hello world'); 
+});
+
+
+
 
 // PORT
 app.listen(port, () => {
