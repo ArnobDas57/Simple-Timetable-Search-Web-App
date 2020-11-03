f61c2711c9805b901177944d3fd0027c39c36028
Arnob Das <adas46@uwo.ca>
2020-11-02 22:25:16 -0500

    created first route for GET request to get subject codes and descriptions

--- index.js
+++ index.js
@@ -15,14 +15,28 @@ const courseData = parsingFunction('Lab3-timetable-data.json');
 // Setup serving front-end code
 app.use('/', express.static('static'));
 
+// Setup middleware to do logging
+app.use((req, res, next) => { // for all routes
+    console.log(`${req.method} request for ${req.url}`);
+    next(); // keep going
+});
 
 // Route for root 
 app.get('/', (req, res) => {
     res.send('Hello world'); 
 });
 
-
-
+// Route to GET subject codes and descriptions -- #1
+app.get('/api/courses', (req, res) => {
+    const subjects = []; 
+    for(i = 0; i < courseData.length; i++) {
+         subjects.push({
+             "subject": courseData[i].subject,
+             "className": courseData[i].className
+            });
+    }
+    res.send(subjects);
+});
 
 // PORT
 app.listen(port, () => {
