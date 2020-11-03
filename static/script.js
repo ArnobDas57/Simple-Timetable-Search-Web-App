1f31915c2a9793623c068dbfd89e6055bc6c9a5b
Arnob Das <adas46@uwo.ca>
2020-11-02 23:06:48 -0500

    Added event listeners to front end buttons and created html divs and tables that appear with button clicks

---
+++ static/script.js
@@ -0,0 +1,5 @@
+document.getElementById('Search-S').addEventListener('click', getResults);
+document.getElementById('Additem').addEventListener('click', createSched);
+document.getElementById('Plus').addEventListener('click', Plus);
+document.getElementById('AddCoursesButton').addEventListener('click', Addcourse);
+document.getElementById('Searchitem').addEventListener('click', getSched);
